78bf967d11f3c88b88e9ebf15faf7665318c4ddd
Andywli <andywli@Andy.local>
2026-01-22 21:10:37 +0800

    更新内容: 添加了新文章/修改了样式

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  // 1. 你的 GitHub Pages 根域名
-  site: 'https://bat9527.github.io', 
-  
-  // 2. 必须与你的新仓库名 "blog" 严格一致，且保留前导斜杠
-  base: '/blog', 
-  
+  // 1. 你的 GitHub Pages 根域名 (现在是自定义域名)
+  site: 'https://andywli.cn',
+
+  // 2. 自定义域名通常映射到根目录，所以 base 设为 '/'
+  base: '/',
+
   integrations: [tailwind()],
 });
